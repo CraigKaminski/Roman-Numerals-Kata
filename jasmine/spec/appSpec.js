a2375a922433ff7a7a1c3e53b2e2afbbac4ab6b9
Craig Kaminski <craig.j.kaminski@gmail.com>
2015-10-19 10:34:21 -0400

    feat: Can now convert numbers 1-9 to roman numerals.

--- jasmine/spec/appSpec.js
+++ jasmine/spec/appSpec.js
@@ -11,5 +11,47 @@ describe('An arabic number', function() {
       var romanNumeral = arabicToRoman(arabicNumber);
       expect(romanNumeral).toBe('II');
     });
+
+    it('returns III when given 3', function() {
+      var arabicNumber = 3;
+      var romanNumeral = arabicToRoman(arabicNumber);
+      expect(romanNumeral).toBe('III');
+    });
+
+    it('returns IV when given 4', function() {
+      var arabicNumber = 4;
+      var romanNumeral = arabicToRoman(arabicNumber);
+      expect(romanNumeral).toBe('IV');
+    });
+
+    it('returns V when given 5', function() {
+      var arabicNumber = 5;
+      var romanNumeral = arabicToRoman(arabicNumber);
+      expect(romanNumeral).toBe('V');
+    });
+
+    it('returns VI when given 6', function() {
+      var arabicNumber = 6;
+      var romanNumeral = arabicToRoman(arabicNumber);
+      expect(romanNumeral).toBe('VI');
+    });
+
+    it('returns VII when given 7', function() {
+      var arabicNumber = 7;
+      var romanNumeral = arabicToRoman(arabicNumber);
+      expect(romanNumeral).toBe('VII');
+    });
+
+    it('returns VIII when given 8', function() {
+      var arabicNumber = 8;
+      var romanNumeral = arabicToRoman(arabicNumber);
+      expect(romanNumeral).toBe('VIII');
+    });
+
+    it('returns IX when given 9', function() {
+      var arabicNumber = 9;
+      var romanNumeral = arabicToRoman(arabicNumber);
+      expect(romanNumeral).toBe('IX');
+    });
   });
 });
--- js/app.js
+++ js/app.js
@@ -2,6 +2,13 @@ function arabicToRoman(arabicNumber) {
   arabicToRomanMap = {
     1: 'I',
     2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII',
+    8: 'VIII',
+    9: 'IX'
   };
   return arabicToRomanMap[arabicNumber];
 }
