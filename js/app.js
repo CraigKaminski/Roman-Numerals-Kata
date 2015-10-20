581f15f3523d9ac4cebeddb7ea409847811f4fb5
Craig Kaminski <craig.j.kaminski@gmail.com>
2015-10-19 20:51:34 -0400

    refactor: Changed logic so arabicToRoman function is now recursive.

--- js/app.js
+++ js/app.js
@@ -1,42 +1,42 @@
-function arabicToRoman(arabicNumber) {
-  var arabicToRomanMap = {
-    1: 'I',
-    2: 'II',
-    3: 'III',
-    4: 'IV',
-    5: 'V',
-    6: 'VI',
-    7: 'VII',
-    8: 'VIII',
-    9: 'IX',
-    10: 'X',
-    20: 'XX',
-    30: 'XXX',
-    40: 'XL',
-    50: 'L',
-    60: 'LX',
-    70: 'LXX',
-    80: 'LXXX',
-    90: 'XC',
-    100: 'C',
-    200: 'CC',
-    300: 'CCC',
-    400: 'CD',
-    500: 'D',
-    600: 'DC',
-    700: 'DCC',
-    800: 'DCCC',
-    900: 'CM',
-    1000: 'M',
-    2000: 'MM',
-    3000: 'MMM'
-  };
+var arabicToRomanMap = {
+  1: 'I',
+  2: 'II',
+  3: 'III',
+  4: 'IV',
+  5: 'V',
+  6: 'VI',
+  7: 'VII',
+  8: 'VIII',
+  9: 'IX',
+  10: 'X',
+  20: 'XX',
+  30: 'XXX',
+  40: 'XL',
+  50: 'L',
+  60: 'LX',
+  70: 'LXX',
+  80: 'LXXX',
+  90: 'XC',
+  100: 'C',
+  200: 'CC',
+  300: 'CCC',
+  400: 'CD',
+  500: 'D',
+  600: 'DC',
+  700: 'DCC',
+  800: 'DCCC',
+  900: 'CM',
+  1000: 'M',
+  2000: 'MM',
+  3000: 'MMM'
+};
 
+function arabicToRoman(arabicNumber) {
   if (arabicNumber < 10 || arabicNumber % 10 === 0) {
     return arabicToRomanMap[arabicNumber];
   } else {
-    var romanNumeral = arabicToRomanMap[Math.floor(arabicNumber / 10) * 10];
-    romanNumeral += arabicToRomanMap[arabicNumber % 10];
-    return romanNumeral;
+    var arabicString = arabicNumber.toString();
+    var greatestPlaceValue = Number(arabicString[0] + '0'.repeat(arabicString.length - 1));
+    return arabicToRomanMap[greatestPlaceValue] + arabicToRoman(arabicNumber - greatestPlaceValue);
   }
 }
