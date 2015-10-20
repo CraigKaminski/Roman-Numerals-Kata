6dce096ec5a5534d5ed4c7cdf383dafda7b5bf5e
Craig Kaminski <craig.j.kaminski@gmail.com>
2015-10-20 14:10:26 -0400

    doc: Added comments to code.

--- js/app.js
+++ js/app.js
@@ -31,14 +31,21 @@ var arabicToRomanMap = {
   3000: 'MMM'
 };
 
+// This function is responsible for converting Arabic numbers to Roman numerals.
 function arabicToRoman(arabicNumber) {
+  // We're unable to represent Roman numerals 4000 or greater. If a number that large
+  // is given, throw an exception.
   if (arabicNumber >= 4000) {
     throw new RangeError('Exceeded Max Value');
+  // If given number is in map, return its value.
   } else if (arabicNumber in arabicToRomanMap) {
     return arabicToRomanMap[arabicNumber];
   } else {
+    // Obtain greatest place value of given number.
     var arabicString = arabicNumber.toString();
     var greatestPlaceValue = Number(arabicString[0] + '0'.repeat(arabicString.length - 1));
+    // Retrieve greatest place value from map and append result of call to conversion
+    // function using given number minus greatest place value as input.
     return arabicToRomanMap[greatestPlaceValue] + arabicToRoman(arabicNumber - greatestPlaceValue);
   }
 }
@@ -76,31 +83,41 @@ var romanToArabicMap = {
   'MMM': 3000
 };
 
+// This function is responsible for converting Roman numerals to Arabic Numbers.
 function romanToArabic(romanNumeral) {
+  // If given numeral is in map, return it.
   if (romanNumeral in romanToArabicMap) {
     return romanToArabicMap[romanNumeral];
   } else {
     var subRomanNumeralFound;
     var sliceEnd = 1;
 
+    // If the first character in given numeral is not in map, throw exception.
     if (! (romanNumeral[0] in romanToArabicMap)) {
       throw new RangeError('Invalid Roman Numeral');
     }
 
+    // Keep expanding substring of given numeral until the substring is no longer
+    // found in map.
     while (romanNumeral.slice(0, sliceEnd) in romanToArabicMap) {
       subRomanNumeralFound = romanNumeral.slice(0, sliceEnd);
       sliceEnd++;
     }
+
     var romanNumeralRemaining = romanNumeral.slice(subRomanNumeralFound.length);
+    // Retrieve substring from map and append the results of call to conversion function
+    // using remainder of given numeral as input.
     return romanToArabicMap[subRomanNumeralFound] + romanToArabic(romanNumeralRemaining);
   }
 }
 
+// Retrieve some DOM elements.
 var arabicInput = document.getElementById('arabicNumber');
 var romanInput = document.getElementById('romanNumeral');
 var arabicToRomanBtn = document.getElementById('arabicToRoman');
 var romanToArabicBtn = document.getElementById('romanToArabic');
 
+// Assign click event handler to Arabic to Roman button.
 arabicToRomanBtn.onclick = function() {
   try {
     romanInput.value = arabicToRoman(parseInt(arabicInput.value));
@@ -109,6 +126,7 @@ arabicToRomanBtn.onclick = function() {
   }
 };
 
+// Assign click event handler to Roman to Arabic button.
 romanToArabicBtn.onclick = function() {
   try {
     arabicInput.value = romanToArabic(romanInput.value);
